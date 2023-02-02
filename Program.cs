c085774cd119b2fbeb136e639dc7ea2537ee6a83
Gandolio1 <81918524+Gandolio1@users.noreply.github.com>
2023-02-02 15:05:54 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ConsoleApplication1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double solution = 7 * Math.Pow(x, 2) + 3 * x + 6;
+            Console.WriteLine("y = " + solution);
+            Console.ReadLine();
+        }
+    }
+}
